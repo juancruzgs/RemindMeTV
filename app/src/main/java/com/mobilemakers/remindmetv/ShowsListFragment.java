96de2338feecf10dbbf68dbfa660b2257f820e41
juancruzgs <juancruz_gs@hotmail.com>
2015-02-16 15:07:10 -0300

    setListAdapter

--- app/src/main/java/com/mobilemakers/remindmetv/ShowsListFragment.java
+++ app/src/main/java/com/mobilemakers/remindmetv/ShowsListFragment.java
@@ -1,16 +1,22 @@
 package com.mobilemakers.remindmetv;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ShowsListFragment extends ListFragment {
+
+    ShowAdapter mAdapter;
 
     public ShowsListFragment() {
     }
@@ -20,5 +26,17 @@ public class ShowsListFragment extends ListFragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_shows_list, container, false);
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        prepareListView();
+    }
+
+    private void prepareListView() {
+        List<Show> shows = new ArrayList<>();
+        mAdapter = new ShowAdapter(getActivity(), shows);
+        setListAdapter(mAdapter);
     }
 }
