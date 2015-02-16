a2a878a01753e7d4cb48e763be90e21a4a69ed84
juancruzgs <juancruz_gs@hotmail.com>
2015-02-16 14:52:29 -0300

    ListView

--- app/src/main/java/com/mobilemakers/remindmetv/ShowsListActivity.java
+++ app/src/main/java/com/mobilemakers/remindmetv/ShowsListActivity.java
@@ -1,15 +1,9 @@
 package com.mobilemakers.remindmetv;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 
 
 public class ShowsListActivity extends ActionBarActivity {
@@ -20,7 +14,7 @@ public class ShowsListActivity extends ActionBarActivity {
         setContentView(R.layout.activity_shows_list);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ShowsListFragment())
                     .commit();
         }
     }
@@ -48,19 +42,4 @@ public class ShowsListActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_shows_list, container, false);
-            return rootView;
-        }
-    }
 }
---
+++ app/src/main/java/com/mobilemakers/remindmetv/ShowsListFragment.java
@@ -0,0 +1,24 @@
+package com.mobilemakers.remindmetv;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ShowsListFragment extends ListFragment {
+
+    public ShowsListFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_shows_list, container, false);
+        return rootView;
+    }
+}
