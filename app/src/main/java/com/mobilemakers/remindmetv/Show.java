962d3f348dd605cac0c9b60576eba0c040da8a98
juancruzgs <juancruz_gs@hotmail.com>
2015-02-16 15:02:45 -0300

    Show and ShowAdapter

---
+++ app/src/main/java/com/mobilemakers/remindmetv/Show.java
@@ -0,0 +1,35 @@
+package com.mobilemakers.remindmetv;
+
+public class Show {
+
+    private String name;
+    private String channel;
+    private String category;
+
+    public Show() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
